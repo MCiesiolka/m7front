cb31cb95f1107424ad61da869db21ed93f3e662d
MCiesiolka <mciesiolka@ivytech.edu>
2025-03-03 20:58:17 -0500

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -12,7 +12,7 @@ async function login(username,password){
         username,
         password
     }
-    const response = await fetch("http://localhost:3000/api/auth",{
+    const response = await fetch("https://mciesiolka.github.io/m7front/api/songs.json",{
         method: "POST",
         headers:{
             "Content-Type" : "application/json"
